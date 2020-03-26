dbd2ec63194aac8858a356b665f8b10b52d8da85
Alexandre Froument <alex.froument@gmail.com>
2020-03-26 11:03:28 +0100

    Modif test

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 import model
+import constants
+import math
 
 m - model.Model()
 
-print ("Default model: {}". format(m))
+m.m1.speed = 10
+m.m2.speed = 10
+print ("First model : {}".format(m))
 
-print ("### Setting same speed for both motors")
-m.m1.speed = 0.1
-m.m2.speed = 0.1
-print ("\n###############\n model: {}". format(m))
 linear_speed, rotational_speed = m.dk()
-print ("linear_speed={}\nrotational_speed={}".format(linear_speed, rotational_speed))
+print("Model : {}".format(m))
+if linear_speed == 10 and rotational_speed == 0:
+    print ("Test1 OK")
+    else: 
+        print("Test1 FAILED")
+
 
 print ("### Setting same speed for both motors")
 m.m1.speed = 0.1
